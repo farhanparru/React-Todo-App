0088469a5726c96c9391b34f7848e2cf7cdce0de
Muhammad Shamin Farhan <140752813+farhanparru@users.noreply.github.com>
2023-10-25 08:48:26 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './TODO APP/Todo';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Todo />
+            
+        </div>
+    );
+}
+
+export default App;
